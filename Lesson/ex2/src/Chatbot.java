1fddf64f3875d833ddae0cdda38c35a0d07f7fce
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-10-25 09:36:50 +0900

    ex2-4

--- Lesson/ex2/src/Chatbot.java
+++ Lesson/ex2/src/Chatbot.java
@@ -2,4 +2,8 @@ public class Chatbot {
     Chatbot() {
         System.out.println("チャットボットの原型を作成します。");
     }
+
+    Chatbot(String name) {
+        System.out.printf("チャットボット名%sを作成します。", name);
+    }
 }
--- Lesson/ex2/src/Main.java
+++ Lesson/ex2/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("main method runs");
         Chatbot bot1 = new Chatbot();
+        Chatbot bot2 = new Chatbot("bot2");
     }
 }
