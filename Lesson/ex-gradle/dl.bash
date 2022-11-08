d5aac991370d3c51012eaa608d146c71784fe0dc
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-11-08 09:25:06 +0900

    ex-gradle

---
+++ Lesson/ex-gradle/dl.bash
@@ -0,0 +1,9 @@
+# ~/prog2/ex-gradle に移動し、
+# gradle init した後で実行することを想定。
+
+curl -O https://raw.githubusercontent.com/naltoma/java_intro2020/master/src/hero_enemy/Main.java
+mv Main.java app/src/main/java/
+curl -O https://raw.githubusercontent.com/naltoma/java_intro2020/master/src/hero_enemy/Hero.java
+mv Hero.java app/src/main/java/jp/ac/uryukyu/ie/e225717/
+curl -O https://raw.githubusercontent.com/naltoma/java_intro2020/master/src/hero_enemy/Enemy.java
+mv Enemy.java app/src/main/java/jp/ac/uryukyu/ie/e225717/
