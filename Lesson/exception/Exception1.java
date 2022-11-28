efdd3d74c2783ac511e0812aec34df5c1e049b2c
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-11-29 08:57:03 +0900

    例外処理練習

---
+++ Lesson/exception/Exception1.java
@@ -0,0 +1,18 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Exception1 {
+	public static void main(String[] args) {
+		// try-catchなしのコード。
+		try {
+			FileWriter fw = new FileWriter("output.txt");
+			String[] results = { "ほげ", "ふが" };
+			for (String temp : results) {
+				fw.write(temp);
+			}
+			fw.close();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+}
