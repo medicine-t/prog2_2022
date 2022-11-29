117c8e3f4d794f9ee31d958fe551834527dc02e2
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-11-29 09:51:23 +0900

    nullpointere exceptionのハンドリング

--- Task/report5/src/App.java
+++ Task/report5/src/App.java
@@ -1,6 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            // 条件2
+            System.err.println("NullPointerexceptionが発生しました");
+            // 条件3
+            System.err.println(e.getMessage());
+        }
+
     }
 }
