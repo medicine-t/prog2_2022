5fdeff3d805bda354dbf6594786c88239bfcc823
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-11-29 09:57:02 +0900

    report5 step3 例外発生確認

--- Task/report5/src/App.java
+++ Task/report5/src/App.java
@@ -1,14 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            // 条件2
-            System.out.println("NullPointerexceptionが発生しました");
-            // 条件3
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
 
     }
 }
