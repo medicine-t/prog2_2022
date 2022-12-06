3a1f5e8e2cab211a1f87ad0ee0b1c36f4a5d83b0
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-12-06 09:52:11 +0900

    HashMapの動作確認

--- Lesson/Week10/src/App.java
+++ Lesson/Week10/src/App.java
@@ -1,16 +1,17 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Set<String> colors = new HashSet<String>();
-        colors.add("赤");
-        colors.add("青");
-        colors.add("黃");
-        System.out.println(colors.size());
-        for (String e : colors) {
-            System.out.println(e);
-        }
+        Map<String, Integer> prefs = new HashMap<String, Integer>();
+        prefs.put("京都", 255);
+        prefs.put("東京", 1261);
+        prefs.put("熊本", 181);
+        System.out.println(prefs.get("東京"));
+        System.out.println(prefs.size());
+        prefs.remove("京都");
+        System.out.println(prefs.size());
+        prefs.put("熊本", 182);
+        System.out.println(prefs.get("熊本"));
+
     }
 }
